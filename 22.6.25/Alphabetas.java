4a0c2d46080830fac9dae376e3090e11a40b120b
Meganathan12 <151717295+Meganathan12@users.noreply.github.com>
2025-06-22 19:32:35 +0530

    Create Alphabetas.java

---
+++ 22.6.25/Alphabetas.java
@@ -0,0 +1,8 @@
+public class PrintAlphabets {
+    public static void main(String[] args) {
+        System.out.println("Alphabets from A to Z:");
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
